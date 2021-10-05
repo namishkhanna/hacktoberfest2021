719272a7800dd84fba00a06ae4503a100e57c5aa
H-M-Noman-123 <>
2021-10-05 16:12:09 +0500

    I write Programm in c++

---
+++ C++/pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    
+    for(int i = 4; i >=1 ; i--){
+        {
+            for(int j = i ; j >=1; j--)
+            cout << "*";
+        }
+        
+            cout << endl;
+        }
+        
+        
+}
+
