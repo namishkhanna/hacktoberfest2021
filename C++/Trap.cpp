1f89883156ef03c02f035617e6077a5d3cbdf948
pranshu98 <51962568+pranshu98@users.noreply.github.com>
2021-10-06 12:59:33 +0530

    Sudoku Solver

---
+++ C++/Trap.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(int A[], int n) {
+        int left=0; int right=n-1;
+        int res=0;
+        int maxleft=0, maxright=0;
+        while(left<=right){
+            if(A[left]<=A[right]){
+                if(A[left]>=maxleft) maxleft=A[left];
+                else res+=maxleft-A[left];
+                left++;
+            }
+            else{
+                if(A[right]>=maxright) maxright= A[right];
+                else res+=maxright-A[right];
+                right--;
+            }
+        }
+        return res;
+    }
+};
+//Question is from leetcode
+//https://leetcode.com/problems/trapping-rain-water/
