74e07b048cac80e0614e27e720a2844233b9dfd7
Abhishek Jaiswal <abhishek.iiitbbsr@gmail.com>
2021-10-10 14:54:24 +0530

    added cocktail algorithm

---
+++ C/cocktail_sort.c
@@ -0,0 +1,58 @@
+// Alogrithm for Cocktail Sort to sort element in ascending order
+
+#include <stdio.h>
+
+int main(void) 
+{
+    int i, j, start=0, n, end, flag;
+    printf ("Enter value of n: ");
+    scanf ("%d", &n);
+    int a[n];
+    printf ("Please enter %d values\n",n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    flag = 0;
+    end = n;
+    while(start <= end)
+    {
+        for(i=start; i<end-1; i++)
+        {
+            if(a[i]>a[i+1])
+            {
+                int temp =a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                flag=1;
+            }
+        }
+        if(flag==0)
+        {
+            break;
+        }
+        flag=0;
+        end = end-1;
+        for(i=end; i>start-1; i--)
+        {
+            if(a[i]<a[i-1])
+            {
+                int temp = a[i];
+                a[i] = a[i-1];
+                a[i-1]= temp;
+                flag=1;
+            }
+        }
+        if(flag==0)
+        {
+            break;
+        }
+        start = start+1;
+        flag=0;
+    }
+    for(i=0; i<n; i++)
+    {
+        printf("%d ", a[i] );
+    }
+    return 0;
+}
