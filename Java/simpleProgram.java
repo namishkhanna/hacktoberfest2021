ba59fdb9c9baa5ca297bba33a2861c4e042de564
florayasmin <55338271+florayasmin@users.noreply.github.com>
2021-10-18 17:00:19 -0700

    Create simpleProgram.java
    Simple program for Hacktoberfest. Please accept.

---
+++ Java/simpleProgram.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int product = 5 * 2;
+    System.out.println(product);
+
+    String name = "Flora";
+    System.out.println(name);
+
+    int expression = 7 + 3;
+    System.out.println(expression);
+   
+    boolean isTrue = false;
+    System.out.println(isTrue);
+  }
+}
