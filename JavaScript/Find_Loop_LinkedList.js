0ed2b8a356fb7963c938ae507b2ec5717e51774d
Maahi <maahicodes@gmail.com>
2021-10-20 18:26:27 +0530

    updated

--- JavaScript/Find_Loop_LinkedList.js
+++ JavaScript/Find_Loop_LinkedList.js
@@ -7,9 +7,9 @@
 let head; //head of list
  
 	class Node {
-		constructor(val) {
-			this.data = val;
-			this.next = null;
+	   constructor(val) {
+		this.data = val;
+		this.next = null;
 		}
 	}
 
@@ -17,43 +17,45 @@ let head; //head of list
 	function push(new_data) {
 
 	// Allocate the Node 
-    let new_node = new Node(new_data);
+        let new_node = new Node(new_data);
 		
-		new_node.next = head; //make next new node as head
+	//make next new node as head
+	new_node.next = head; 
 		
-		head = new_node; // Move the head to point to new Node
+	// Move the head to point to new Node
+	head = new_node; 
 	}
 
 	// returns true if there is a loop in linked else false
 	function detectLoop(h) {
-		let s = new Set();
-		while (h != null) {
+	  let s = new Set();
+	   while (h != null) {
 
-			// If already have this node in hashmap it means their is a cycle
-			if (s.has(h)){
-				return true;
+	    // If already have this node in hashmap
+	    if (s.has(h)){
+	    return true;
             }
-			// If node is new, insert it in hash
-			s.add(h);
+		   
+	    // If node is new, insert it in hash
+	   s.add(h);
+           h = h.next;
+	  }
 
-			h = h.next;
-		}
-
-		return false;
-	}
+	   return false;
+       }
 
 	// to test above function
-		push(21);
-		push(8);
-		push(12);
-		push(10);
+	push(21);
+	push(8);
+	push(12);
+	push(10);
 
-		/* Create loop for testing */
-		head.next.next.next.next = head;
+	// Create loop for testing
+	head.next.next.next.next = head;
 
-		if (detectLoop(head)){
-			console.log("Loop found");
-		}
-		else {
-			console.log("No Loop");
-}
+	if (detectLoop(head)){
+	  console.log("Loop found");
+	}
+	else {
+	  console.log("No Loop");
+        }
